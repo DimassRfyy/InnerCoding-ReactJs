b13e70f6abaf381657107329ba3ee6c3bcba29d4
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2024-11-26 12:17:28 +0700

    Add Banner, Course, Partner, Tutors, and Contact components; update Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import Banner from "../components/Banner"
+import Contact from "../components/Contact"
+import Course from "../components/Course"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Partner from "../components/Partner"
+import Tutors from "../components/Tutors"
 
 function Home() {
   return (
     <>
       <Navbar />
+      <div className="wrapper">
+      <Banner />
+      <Course />
+      <Tutors />
+      <Partner />
+      </div>
+      <Contact />
       <Footer/>
     </>
   )
